ce02936c33c4f9cbc9723aa0fa41f2052dff373c
qwerty935 <isha26701@gmail.com>
2020-06-29 17:23:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -37,5 +37,24 @@ function turn(squareId, player) {
 }
 
 function checkWin(board,player) {
-  
+  let plays = board.reduce((a,e,i) => 
+  (e === player)? a.concat(i) : a,[]);
+  let gameWon = null;
+  for (let [index, win] of winCombos.entries()){
+    if (win.every(elem => plays.indexOf(elem)>-1)){
+      gameWon = {index: index , player: player};
+      break;
+    }
+  }
+  return gameWon;
 }
+
+function gameOver(gameWon){
+  for (let index of winCombos[gameWon.index]){
+    document.getElementById(index).style.backgroundColor=gameWon.player==huPlayer?"blue":"red"
+  }
+  for (var i=0; i<cells.length;i++){
+    cells[i].removeEventListener('click',turnClick,false)
+  }
+}
+
